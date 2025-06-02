65a64ba410b938c57130547130f23a669b534f58
Subhash2615 <120612128+Subhash2615@users.noreply.github.com>
2025-06-02 11:31:33 +0530

    model.py

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.layers import  Flatten, Dense, Dropout
 from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
 
